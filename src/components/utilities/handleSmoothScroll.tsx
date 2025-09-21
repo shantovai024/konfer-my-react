db6cf1677021f3b7393e6dadfd0afce4809ad52e
istiak <istiakahmed024@gmail.com>
2025-09-21 17:58:31 +0600

    first commit

---
+++ src/components/utilities/handleSmoothScroll.tsx
@@ -0,0 +1,8 @@
+import type { MouseEvent } from 'react';
+
+const handleSmoothScroll = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    window.scrollTo({ behavior: 'smooth', top: 0 });
+};
+
+export default handleSmoothScroll;
