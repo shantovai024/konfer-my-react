b4578f0b8eb09809db874b33d28bafa70aa67749
istiak <istiakahmed024@gmail.com>
2025-10-01 18:58:51 +0600

    test

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,11 +3,3 @@ declare module "*.jsx" {
   const component: FC<Record<string, unknown>>;
   export default component;
 }
-
-
-// types/json.d.ts
-declare module "*.json" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const value: any;
-  export default value;
-}
