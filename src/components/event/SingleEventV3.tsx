28309059e79d606f28cbdb00eccf227cac1b3934
istiak <istiakahmed024@gmail.com>
2025-10-03 16:26:21 +0600

    all issue fixed

--- src/components/event/SingleEventV3.tsx
+++ src/components/event/SingleEventV3.tsx
@@ -17,42 +17,58 @@ interface DataType {
 const SingleEventV3 = ({ block }: { block: DataType }) => {
     const { id, blockClass, image, city, title, speaker, role, time, description, button1Class, thumb } = block
 
+    const contentBox = (
+        <div className="content-box">
+            <div className="city">
+                <i className="icon fa fa-map-marker-alt" /> {city}
+            </div>
+            <h3 className="title">
+                <Link to={`/event-single/${id}`}>{title}</Link>
+            </h3>
+            <ul className="location-box">
+                <li><i className="icon fa fa-microphone" />{speaker} <span>({role})</span></li>
+                <li className="date"><i className="icon fa fa-clock" />{time}</li>
+            </ul>
+            <div className="text">{description}</div>
+            <div className="btn-box">
+                <Link to={`/event-single/${id}`} className={`theme-btn btn-style-one ${button1Class}`}>
+                    <span className="btn-title">View More</span>
+                </Link>
+                <Link to="/contact" className="theme-btn btn-style-one bg-orange">
+                    <span className="btn-title">Buy Ticket</span>
+                </Link>
+            </div>
+            <div className="thumb">
+                <img src={`/images/resource/${thumb}`} alt="Image" />
+            </div>
+        </div>
+    );
+
+    const imageBox = (
+        <div className="image-box">
+            <figure className="image">
+                <Link to={`/event-single/${id}`}>
+                    <img src={`/images/resource/${image}`} alt="Image" />
+                </Link>
+            </figure>
+        </div>
+    );
+
     return (
         <>
             <div className={`event-block-three has-active ${blockClass ? blockClass : ""}`}>
                 <div className="inner-box">
-                    <div className="image-box">
-                        <figure className="image">
-                            <Link to={`/event-single/${id}`}>
-                                <img src={`/images/resource/${image}`} alt="Image" />
-                            </Link>
-                        </figure>
-                    </div>
-                    <div className="content-box">
-                        <div className="city">
-                            <i className="icon fa fa-map-marker-alt" /> {city}
-                        </div>
-                        <h3 className="title">
-                            <Link to={`/event-single/${id}`}>{title}</Link>
-                        </h3>
-                        <ul className="location-box">
-                            <li><i className="icon fa fa-microphone" />{speaker} <span>({role})</span></li>
-                            <li className="date"><i className="icon fa fa-clock" />{time}</li>
-                        </ul>
-                        <div className="text">{description}</div>
-                        <div className="btn-box">
-
-                            <Link to={`/event-single/${id}`} className={`theme-btn btn-style-one ${button1Class}`}><span className="btn-title">View More</span></Link>
-
-                            <Link to="/contact" className="theme-btn btn-style-one bg-orange">
-                                <span className="btn-title">Buy Ticket</span>
-                            </Link>
-
-                        </div>
-                        <div className="thumb">
-                            <img src={`/images/resource/${thumb}`} alt="Image" />
-                        </div>
-                    </div>
+                    {blockClass === "right" ? (
+                        <>
+                            {contentBox}
+                            {imageBox}
+                        </>
+                    ) : (
+                        <>
+                            {imageBox}
+                            {contentBox}
+                        </>
+                    )}
                 </div>
             </div>
         </>
